eaa51112df1af323f210d0b8b08dbb94cc2f1c67
Suryakanta Prusty <kantaprustys@gmail.com>
2025-05-03 01:20:34 +0530

    added the cors link

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,25 @@ mongoose
     console.error("MongoDB connection error:", err);
   });
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:4200",
+  "https://sync-beat.vercel.app",
+];
+
+// Apply CORS to Express
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use(express.json()); // Important: handle JSON requests
 app.use("/api/auth", authRoutes);
 
