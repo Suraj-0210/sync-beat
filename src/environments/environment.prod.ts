13af73b4ac5cb811cdbb8042cb36dc99f6c522b1
Suryakanta Prusty <kantaprustys@gmail.com>
2025-06-04 17:58:01 +0530

    updated the backend uri

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   firebaseApiKey: 'AIzaSyDTsvxRi12LgzoWpydbErOI3TNdIk5YRd4',
-  backendUrl: 'https://sync-beat.onrender.com',
+  backendUrl:
+    'https://sync-beat-hefhdzfmcteug6gm.eastasia-01.azurewebsites.net/',
 };
