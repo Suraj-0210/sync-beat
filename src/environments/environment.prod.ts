20b81e63ba888964fb47a34040dbd7df5970fd98
Suryakanta Prusty <kantaprustys@gmail.com>
2025-06-04 18:10:41 +0530

    updated the uri

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   firebaseApiKey: 'AIzaSyDTsvxRi12LgzoWpydbErOI3TNdIk5YRd4',
   backendUrl:
-    'https://sync-beat-hefhdzfmcteug6gm.eastasia-01.azurewebsites.net/',
+    'https://sync-beat-hefhdzfmcteug6gm.eastasia-01.azurewebsites.net',
 };
